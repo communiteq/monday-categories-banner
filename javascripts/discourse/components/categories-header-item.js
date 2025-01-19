a48697b4d18df35049eb20e9aab9a37f98ae2636
Richard <richard@communiteq.com>
2025-01-19 21:36:50 +0000

    Fix: pass category argument

--- javascripts/discourse/components/categories-header-item.js
+++ javascripts/discourse/components/categories-header-item.js
@@ -1,5 +1,11 @@
 import Component from '@glimmer/component';
-
+import { tracked } from '@glimmer/tracking';
 
 export default class CategoriesHeaderItem extends Component {
+  @tracked category = [];
+
+  constructor() {
+      super(...arguments);
+      this.category = this.args.category;
+  }
 }
